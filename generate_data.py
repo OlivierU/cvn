8d8827782d7cd2f0643130bd1b4560958cc4d665
Olivier <olivier.ulrich@students.unibe.ch>
2021-11-15 17:49:48 +0100

    added a basic function to generate some simulated data following a second order polynomial function

--- generate_data.py
+++ generate_data.py
@@ -1,11 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from math import exp
 from scipy.stats import norm
 
 x = np.arange(0, 100)
-dist = [0.44*exp(.031*_) for _ in x]
-error = norm.rvs(0, scale=.95, size=100)
+dist = [0.44*_*_ + 0.13*_ + 41 for _ in x]
+error = norm.rvs(0, scale=189, size=100)
 
 y = np.asanyarray(dist+error)
 
