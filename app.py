ab557c684add5c61f5156405705f232d1a35a64f
olivierulrich <olivier.ulrich@bluewin.ch>
2021-11-22 12:43:04 +0100

    added descriptive comments

--- app.py
+++ app.py
@@ -2,14 +2,48 @@ from sample import Sample
 from model import Model
 from matplotlib import pyplot as plt
 
-# define parameters to generate a sample
-size = 100
-parameters = {
+# ***********************************************************************
+# This file contains the main logic of the modeling process.
+#
+# All necessary variables are defined and used to build the objects needed to do the actual calculations.
+# The basic structure is:
+#   - Generate a random sample based on a specified distribution and it's parameters
+#   - Define a model and calculate likelihoods for a set range of parameters
+#   - Generate plots to visualise the process
+#
+# ***********************************************************************
+
+
+########################
+# Variable definitions #
+########################
+
+# Sample #
+
+size = 100 # number of values to generate
+
+parameters = { # distribution parameters used
     "a": 0.44,
     "b": 0.13,
     "m": 41
 }
-error = 65
+
+error = 65 # SD of the error added to each value
+
+# Optimisation #
+
+limits = { # computational range for each parameter
+    "a": [0.3, 0.5],
+    "b": [0.1, 0.3],
+    "m": [1, 100]
+}
+
+steps = 20 # number of optimisation cycles
+
+
+#######################
+# Object Construction #
+#######################
 
 # generate sample data based on given params
 sample = Sample(size, parameters, error)
@@ -17,19 +51,21 @@ sample = Sample(size, parameters, error)
 data = sample.get_sample()
 sample.save_sample(data)
 
+# generate the model used to find parameters
 model = Model()
 
-# define a computational range for each parameter
-limits = {
-    "a": [0.3, 0.5],
-    "b": [0.1, 0.3],
-    "m": [1, 100]
-}
 
-# define the number of steps to use for optimisation
-steps = 20
+################
+# Optimisation #
+################
 
+# find maximum likelihood function based on the defined variables
 abm_max = model.optimise(data, steps, limits)
+
+
+############
+# Plotting #
+############
 
 # plot the sample data as well as the maximum likelihood model
 plt.plot(data["x"], data["y"], 'r.')
